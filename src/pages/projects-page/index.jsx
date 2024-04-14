fb2d67bddf688a9caca66b69e1a6fe01a6e48351
Helen Burger <burger.helen.m@gmail.com>
2024-04-13 23:03:14 -0700

    Add skeleton content to projects page

--- src/pages/projects-page/index.jsx
+++ src/pages/projects-page/index.jsx
@@ -1,7 +1,32 @@
+import { Title, Text, Card, Image, Box, Group } from '@mantine/core';
+import Footer from '../../components/Footer';
+
 export default function Projects() {
   return (
     <div id="projects">
-      <h1>Projects Page!</h1>
+      <Title order={1}>Projects Library</Title>
+      {/* Filter? */}
+      <Card padding="lg" radius="md" withBorder>
+        <Box>
+          <Title order={3}>Project Name</Title>
+          <Text>HTML | SCSS | JQuery</Text>
+        </Box>
+        <Card.Section withBorder>
+          <Image src="" height={160} alt="an image" />
+        </Card.Section>
+        <Box>
+          <Text c={'dimmed'}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit
+            ullam, voluptatibus magnam obcaecati eos, nemo odit iusto sed soluta
+            nesciunt ipsum veniam modi placeat!
+          </Text>
+          <Group justify="space-between">
+            <Text>Launch</Text>
+            <Text>Read</Text>
+          </Group>
+        </Box>
+      </Card>
+      <Footer />
     </div>
   );
 }
