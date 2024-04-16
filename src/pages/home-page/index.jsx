8bc6b68ae68c5a511512cad0079e82353267c667
Helen Burger <burger.helen.m@gmail.com>
2024-04-15 21:46:22 -0700

    Add basic layout to home page hero section

--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -1,4 +1,4 @@
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Box } from '@mantine/core';
 import Footer from '../../components/Footer';
 import FeaturedProject from './featured-project';
 import classes from '../../styles/Home.module.css';
@@ -6,8 +6,11 @@ import classes from '../../styles/Home.module.css';
 export default function Home() {
   return (
     <div id="home">
-      <Title order={1}>Helen Burger</Title>
-      <Text>Designer | Developer</Text>
+      <Box className={classes.hero}>
+        <div className={classes.circle}></div>
+        <Title order={1}>Helen Burger</Title>
+        <Text>Designer | Developer</Text>
+      </Box>
       <section id="featured-projects">
         <Title order={2}>Featured Projects</Title>
         {/* TODO: rewrite as a loop */}
