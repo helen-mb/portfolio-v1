d5fd008b313a3c892536e906a7a592f7bacb8a79
Helen Burger <burger.helen.m@gmail.com>
2024-04-17 15:54:34 -0700

    Add some more style and aria label to mobile nav

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,10 +1,13 @@
 import { Affix, Tabs, Stack, Text } from '@mantine/core';
+import { useWindowScroll } from '@mantine/hooks';
 import { IconHome, IconBooks, IconUser, IconMail } from '@tabler/icons-react';
 import { useNavigate, useParams } from 'react-router-dom';
+import classes from '../styles/MobileNav.module.css';
 
 function MobileNav() {
   const navigate = useNavigate();
   const { tabValue } = useParams();
+  const [scroll, scrollTo] = useWindowScroll();
 
   return (
     <Affix hiddenFrom="xs" position={{ bottom: 0, left: 0, right: 0 }}>
@@ -12,27 +15,28 @@ function MobileNav() {
         activateTabWithKeyboard={false}
         value={tabValue}
         onChange={(value) => navigate(`/${value}`)}
+        classNames={classes}
       >
-        <Tabs.List grow justify="center">
-          <Tabs.Tab value="home">
+        <Tabs.List grow justify="center" aria-label="Main Menu">
+          <Tabs.Tab value="home" onClick={() => scrollTo({ y: 0 })}>
             <Stack align="center" gap={'0'}>
               <IconHome />
               <Text size="xs">Home</Text>
             </Stack>
           </Tabs.Tab>
-          <Tabs.Tab value="projects">
+          <Tabs.Tab value="projects" onClick={() => scrollTo({ y: 0 })}>
             <Stack align="center" gap={'0'}>
               <IconBooks />
               <Text size="xs">Projects</Text>
             </Stack>
           </Tabs.Tab>
-          <Tabs.Tab value="about">
+          <Tabs.Tab value="about" onClick={() => scrollTo({ y: 0 })}>
             <Stack align="center" gap={'0'}>
               <IconUser />
               <Text size="xs">About</Text>
             </Stack>
           </Tabs.Tab>
-          <Tabs.Tab value="contact">
+          <Tabs.Tab value="contact" onClick={() => scrollTo({ y: 0 })}>
             <Stack align="center" gap={'0'}>
               <IconMail />
               <Text size="xs">Contact</Text>
