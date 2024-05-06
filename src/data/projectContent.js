94cec55de277dab09e5d261d72f74541ee160e91
Helen Burger <burger.helen.m@gmail.com>
2024-05-05 23:07:26 -0700

    Generate mock content for the project details page

--- src/data/projectContent.js
+++ src/data/projectContent.js
@@ -1,20 +1,68 @@
 export const projectContents = [
 	{
-			id: 1,
-			title: "Take Five: Movie DB",
-			stack: "Design | React| Chakra UI| TMDB API",
-			url: "https://take-five.helenburger.com/"
-		},
-		{
-			id: 2,
-			title: "Hunt the Wumpus",
-			stack: "JQuery | JavaScript",
-			url: "https://hunt-the-wumpus.helenburger.com/"
-		},
-		{
-			id: 3,
-			title: "Visit Wonderland",
-			stack: "Design | HTML | CSS | JS",
-			url: "https://helenburger.com/fwd-wonderland/"
-		}
+		id: 0,
+		title: "Take Five: Movie DB",
+		stack: "Design | React| Chakra UI| API",
+		url: "https://take-five.helenburger.com/",
+		git: "https://github.com/helen-mb/fwd-mdb",
+		intro: "From conceptualization, wireframing, and a high-fidelity Photoshop mockup, to a functional React project using Chakra UI and the TMDB database.",
+		content: [
+			[
+				'paragraph', "This application started as a solo design project. I started with conceptualization using hand-drawn lo-fidelity wireframes, and eventually created a hi-fidelity mockup in Adobe Photoshop. I then joined a team of three, and together we built a fully functional prototype in Adobe XD, before finally building the application in React."
+			],
+			[
+				'image', 'testImage'
+			],
+			[
+				'paragraph', "This is another paragraph"
+			],
+			[
+				'image', 'testImage'
+			],
+		]
+	},
+	{
+		id: 1,
+		title: "Hunt the Wumpus",
+		stack: "JQuery | JavaScript",
+		url: "https://hunt-the-wumpus.helenburger.com/",
+		git: "https://github.com/helen-mb/fwd-wumpus",
+		intro: "A visual exploration of the classic text-based game, Hunt the Wumpus, built using Vanilla JS and JQuery. Features traversal of a dodecahedron-shaped space.",
+		content: [
+			[
+				'paragraph', "For this game, I wanted to try something with slightly more challenging logic than a basic game of rock-paper-scissors. I spent a lot of time preparing the logic by hand and making diagrams to understand what information would be needed when."
+			],
+			[
+				'image', 'testImage'
+			],
+			[
+				'paragraph', "This is another paragraph"
+			],
+			[
+				'image', 'testImage'
+			],
+		]
+	},
+	{
+		id: 2,
+		title: "Visit Wonderland",
+		stack: "Design | HTML | CSS | JS",
+		url: "https://helenburger.com/fwd-wonderland/",
+		git: "https://github.com/helen-mb/fwd-wonderland",
+		intro: "A mock travel website serving as a personal design challenge and a demonstration of HTML, CSS, and JS skills.",
+		content: [
+			[
+				'paragraph', "For this project, we were expected to make a simple 'country' website with which to practice our HTML and CSS skills. I wanted, however, to try my hand at designing something that one would be more likely to see in the real world. What about a travel website?"
+			],
+			[
+				'image', 'testImage'
+			],
+			[
+				'paragraph', "This is another paragraph"
+			],
+			[
+				'image', 'testImage'
+			],
+		]
+	}
 ];
--- src/pages/details-page/index.jsx
+++ src/pages/details-page/index.jsx
@@ -4,14 +4,21 @@ import {
   Stack,
   Text,
   Center,
+  Anchor,
   BackgroundImage,
   Image,
   Flex,
 } from '@mantine/core';
+import { IconTerminal2, IconExternalLink } from '@tabler/icons-react';
+import { useParams } from 'react-router-dom';
+import { projectContents } from '../../data/projectContent';
 import classes from '../../styles/Details.module.css';
 import IMAGES from '../../images/Images';
 
 export default function Details() {
+  const { id } = useParams();
+  const projectDetails = projectContents[id];
+
   return (
     <div id="details">
       <Box className={classes.hero}>
@@ -26,25 +33,32 @@ export default function Details() {
           <Center h={'100vh'}>
             <div className={classes.circle}>
               <Center>
-                <Stack gap={'xs'}>
-                  <Title order={2}>Project Title</Title>
-                  <Text>Project Subtitle</Text>
+                <Stack gap={'xs'} align="center">
+                  <Title order={2}>{projectDetails.title}</Title>
+                  <Text>{projectDetails.stack}</Text>
                 </Stack>
               </Center>
             </div>
           </Center>
         </Stack>
       </Box>
-      <Title order={2}>Project Title</Title>
-      <Text>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Necessitatibus, numquam nam at corporis, atque delectus ab quo minima
-        tenetur adipisci rerum consequatur eius amet inventore, omnis tempore?
-        Magnam, harum tempore reprehenderit modi ex, inventore consectetur
-        beatae similique explicabo illo voluptatibus. Non, corrupti, sint
-        officia minima pariatur quod sapiente praesentium repellendus illum ad
-        aliquam sed dolor!
-      </Text>
+      <Box className={classes.content}>
+        <Text className={classes.xlIntroText}>{projectDetails.intro}</Text>
+        <Anchor href={projectDetails.git} target="_blank">
+          <IconTerminal2 /> See the Code
+        </Anchor>
+        <Anchor href={projectDetails.url} target="_blank">
+          <IconExternalLink /> Launch the Project
+        </Anchor>
+        {projectDetails.content.map((contentItem, index) => {
+          if (contentItem[0] === 'paragraph') {
+            return <Text key={index}>{contentItem[1]}</Text>;
+          } else if (contentItem[0] === 'image') {
+            let imageKey = contentItem[1];
+            return <Image src={IMAGES[imageKey]} key={index} />;
+          }
+        })}
+      </Box>
     </div>
   );
 }
