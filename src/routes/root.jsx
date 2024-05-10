eed6187a2c0443348785d8d16058afe3fbc579d1
Helen Burger <burger.helen.m@gmail.com>
2024-05-09 21:00:53 -0700

    Add a playful cursor. Might change

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,16 +2,25 @@ import MobileNav from '../components/MobileNav';
 import HeaderNav from '../components/HeaderNav';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router-dom';
+import { useMouse } from '@mantine/hooks';
+import { useRef } from 'react';
 
 export default function Root() {
+  const { x, y } = useMouse();
+  const cursor = useRef(null);
+  const changePosition = () => {
+    cursor.current.style.top = `${y}px`;
+    cursor.current.style.left = `${x}px`;
+  };
   return (
-    <>
+    <div onMouseMove={changePosition}>
       <HeaderNav />
       <MobileNav />
       <div id="content">
+        <div className="cursor" ref={cursor}></div>
         <Outlet />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
